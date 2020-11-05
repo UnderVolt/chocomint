d4e373114b1ba68408e792b03285319a4777fd6f
Gerardo Wacker <32494885+HeladoDeMascarpone@users.noreply.github.com>
2020-11-04 22:07:13 -0300

    Render notification panel only if active

--- src/main/java/io/undervolt/gui/notifications/NotificationPanel.java
+++ src/main/java/io/undervolt/gui/notifications/NotificationPanel.java
@@ -10,7 +10,8 @@ public class NotificationPanel extends Gui {
 
     private final Minecraft mc;
     private boolean isActive;
-    private NotificationManager notificationManager;
+
+    private final NotificationManager notificationManager;
 
     public NotificationPanel(final Minecraft mc, boolean isActive, final NotificationManager notificationManager) {
         this.mc = mc;
@@ -19,19 +20,21 @@ public class NotificationPanel extends Gui {
     }
 
     public void drawPanel(int screenWidth, int screenHeight) {
-        drawRect( screenWidth - 100, 24,
-                screenWidth, screenHeight, new Color(34, 34, 34, 183).getRGB());
-        drawString(this.mc.fontRendererObj, "Notificaciones", screenWidth - 95,
-                29, Color.WHITE.getRGB());
-        AtomicInteger x = new AtomicInteger();
-        this.notificationManager.getNotifications().forEach(notification -> {
-            drawRect(screenWidth - 95, 40 + x.get(),
-                    screenWidth - 5, 60 + x.get(), Color.WHITE.getRGB());
-            this.mc.fontRendererObj.drawString(notification.title, screenWidth - 93,
-                    42 + x.get(), Color.BLACK.getRGB()
-            );
-            x.set(x.get() + 28);
-        });
+        if(this.isActive) {
+            drawRect(screenWidth - 120, 20,
+                    screenWidth, screenHeight, new Color(34, 34, 34, 183).getRGB());
+            drawString(this.mc.fontRendererObj, "Notificaciones", screenWidth - 115,
+                    29, Color.WHITE.getRGB());
+            AtomicInteger x = new AtomicInteger();
+            this.notificationManager.getNotifications().forEach(notification -> {
+                drawRect(screenWidth - 115, 40 + x.get(),
+                        screenWidth - 5, 60 + x.get(), Color.WHITE.getRGB());
+                this.mc.fontRendererObj.drawString(notification.title, screenWidth - 113,
+                        42 + x.get(), Color.BLACK.getRGB()
+                );
+                x.set(x.get() + 28);
+            });
+        }
     }
 
     public boolean isActive() {
@@ -40,5 +43,10 @@ public class NotificationPanel extends Gui {
 
     public void toggleActive() {
         this.isActive = !this.isActive;
+    }
+
+
+    public void setActive(boolean active) {
+        isActive = active;
     }
 }
