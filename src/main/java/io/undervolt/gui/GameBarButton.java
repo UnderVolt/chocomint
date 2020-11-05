24a4cf25362007edb7652c7b70893e4ecb93c53a
Gerardo Wacker <32494885+HeladoDeMascarpone@users.noreply.github.com>
2020-11-04 21:09:10 -0300

    Added buttons to GameBar

---
+++ src/main/java/io/undervolt/gui/GameBarButton.java
@@ -0,0 +1,11 @@
+package io.undervolt.gui;
+
+import net.minecraft.client.gui.GuiButton;
+
+public class GameBarButton extends GuiButton {
+
+    public GameBarButton(int buttonId, int x, int y, int widthIn, int heightIn, String buttonText) {
+        super(buttonId, x, y, widthIn, heightIn, buttonText);
+    }
+    
+}
