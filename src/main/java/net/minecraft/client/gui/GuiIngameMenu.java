6a2e1985214acf3d8e63d64a22ccb6fb364e8406
GerardoWacker <g@mscpn.com>
2020-11-22 23:16:11 -0300

    Implemented GameBar into GuiIngameMenu

--- src/main/java/net/minecraft/client/gui/GuiIngameMenu.java
+++ src/main/java/net/minecraft/client/gui/GuiIngameMenu.java
@@ -1,16 +1,28 @@
 package net.minecraft.client.gui;
 
 import java.io.IOException;
+
+import io.undervolt.gui.GameBar;
+import io.undervolt.instance.Chocomint;
 import net.minecraft.client.gui.achievement.GuiAchievements;
 import net.minecraft.client.gui.achievement.GuiStats;
 import net.minecraft.client.multiplayer.WorldClient;
 import net.minecraft.client.resources.I18n;
 import net.minecraft.realms.RealmsBridge;
 
-public class GuiIngameMenu extends GuiScreen
+public class GuiIngameMenu extends GameBar
 {
     private int field_146445_a;
     private int field_146444_f;
+
+    /**
+     * Constructor
+     *
+     * @param chocomint
+     */
+    public GuiIngameMenu(final Chocomint chocomint) {
+        super(null, chocomint);
+    }
 
     /**
      * Adds the buttons (and other controls) to the screen in question. Called when the GUI is displayed and when the
@@ -36,6 +48,7 @@ public class GuiIngameMenu extends GuiScreen
         this.buttonList.add(new GuiButton(5, this.width / 2 - 100, this.height / 4 + 48 + i, 98, 20, I18n.format("gui.achievements", new Object[0])));
         this.buttonList.add(new GuiButton(6, this.width / 2 + 2, this.height / 4 + 48 + i, 98, 20, I18n.format("gui.stats", new Object[0])));
         guibutton.enabled = this.mc.isSingleplayer() && !this.mc.getIntegratedServer().getPublic();
+        super.initGui();
     }
 
     /**
@@ -91,6 +104,7 @@ public class GuiIngameMenu extends GuiScreen
             case 7:
                 this.mc.displayGuiScreen(new GuiShareToLan(this));
         }
+        super.actionPerformed(button);
     }
 
     /**
@@ -105,10 +119,9 @@ public class GuiIngameMenu extends GuiScreen
     /**
      * Draws the screen and all the components in it. Args : mouseX, mouseY, renderPartialTicks
      */
-    public void drawScreen(int mouseX, int mouseY, float partialTicks)
-    {
-        this.drawDefaultBackground();
-        this.drawCenteredString(this.fontRendererObj, I18n.format("menu.game", new Object[0]), this.width / 2, 40, 16777215);
+    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
         super.drawScreen(mouseX, mouseY, partialTicks);
     }
+
+
 }
