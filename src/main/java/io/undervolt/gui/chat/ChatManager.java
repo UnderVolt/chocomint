776718b8a5ea9de332f3bdb2c23135b2149848e0
Gerardo Wacker <ge@rar.vg>
2020-12-04 00:02:01 -0300

    Added Chat tabs filtering

--- src/main/java/io/undervolt/gui/chat/ChatManager.java
+++ src/main/java/io/undervolt/gui/chat/ChatManager.java
@@ -1,25 +1,40 @@
 package io.undervolt.gui.chat;
 
 import com.google.common.collect.Lists;
+import io.undervolt.instance.Chocomint;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ChatManager {
 
-    private final Tab reservedServerTab = new Tab(true, "server_reserved", -1);
-    private final Tab reservedLogTab = new Tab(true, "#debug-log", -1);
+    private final Chocomint chocomint;
+    private final Tab reservedServerTab = new Tab(true, "server_reserved", -1, true);
+    private final Tab reservedLogTab = new Tab(true, "#debug-log", -1, true);
+    private final List<Tab> openTabs = Lists.newArrayList(reservedServerTab);
+
+    public ChatManager(final Chocomint chocomint) {
+        this.chocomint = chocomint;
+    }
+
 
     public Tab getReservedServerTab() {
         return reservedServerTab;
     }
 
-    private List<Tab> openTabs = Lists.newArrayList(
-            reservedServerTab, reservedLogTab,
-            new Tab(true, "#general", 2)
-    );
-
     public List<Tab> getOpenTabs() {
         return openTabs;
+    }
+
+    public Tab getOrCreateTabByName(final String tabName) {
+        final Tab tab;
+        final List<Tab> filteredTabs = this.openTabs.stream().filter(t -> t.getName().equals(tabName)).collect(Collectors.toList());
+        if(filteredTabs.get(0) != null) tab = filteredTabs.get(0);
+        else {
+            tab = new Tab(true, tabName, 0, false);
+            this.openTabs.add(tab);
+        }
+        return tab;
     }
 
     public void addTab(final Tab tab) {
