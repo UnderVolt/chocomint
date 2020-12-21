8a360941b455655dfcb5757acf066b296b2bb165
Gerardo Wacker <g@mscpn.com>
2020-12-21 14:08:44 -0300

    Added UserSearch functionality

--- src/main/java/io/undervolt/gui/user/UserSearch.java
+++ src/main/java/io/undervolt/gui/user/UserSearch.java
@@ -1,6 +1,7 @@
 package io.undervolt.gui.user;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import io.undervolt.api.almendra.Almendra;
 import io.undervolt.gui.chat.AvailableRooms;
 import io.undervolt.gui.chat.Chat;
@@ -16,6 +17,7 @@ import net.minecraft.client.gui.GuiTextField;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
@@ -24,12 +26,12 @@ public class UserSearch extends Menu {
     private final Chocomint chocomint;
     private final ChatManager chatManager;
     private final Almendra almendra;
-    private final List<UserCard> filteredUserList = Lists.newArrayList();
+    private final Map<String, UserCard> filteredUserMap = Maps.newHashMap();
 
     private GuiTextField textField;
 
     public UserSearch(final GuiScreen previous, final Chocomint chocomint) {
-        super(previous, chocomint, "Enviar un mensaje privado", 0);
+        super(previous, chocomint, "Buscar un usuario", 0);
         this.chocomint = chocomint;
         this.previous = previous;
         this.chatManager = chocomint.getChatManager();
@@ -39,8 +41,8 @@ public class UserSearch extends Menu {
     @Override
     public void initGui() {
 
-        this.textField = new GuiTextField(0, this.fontRendererObj, this.width / 2 - 100,
-                22, 200, 23);
+        this.textField = new GuiTextField(0, this.fontRendererObj, 20,
+                25, this.width - 40, 20);
 
         this.textField.setFocused(true);
         super.initGui();
@@ -50,14 +52,12 @@ public class UserSearch extends Menu {
     public void drawMenuItems(int mouseX, int mouseY, float partialTicks) {
         this.textField.drawTextBox();
 
-        if(this.filteredUserList != null) {
-            AtomicInteger y = new AtomicInteger(50);
-            AtomicInteger x = new AtomicInteger(2);
-            this.filteredUserList.forEach(u -> {
-                    u.drawCard(this.width / 2 - 65, y.get());
-                y.set(y.get() + 43);
-            });
-        }
+        AtomicInteger y = new AtomicInteger(60);
+        AtomicInteger x = new AtomicInteger(2);
+        this.filteredUserMap.forEach((u, c) -> {
+                c.drawCard(this.width / 2 - 65, y.get());
+            y.set(y.get() + 43);
+        });
     }
 
     @Override
@@ -69,31 +69,15 @@ public class UserSearch extends Menu {
     @Override
     protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
 
-        if(filteredUserList.size() > 0) {
-            for (UserCard userCard : this.filteredUserList) {
+        if(filteredUserMap.size() > 0) {
+            this.filteredUserMap.forEach((user, userCard) -> {
                 if (mouseX >= userCard.x && mouseY >= userCard.y && mouseX <= userCard.x + 130 && mouseY <= userCard.y + 38) {
                     userCard.getConsumer().accept(userCard.getUser());
                 }
-            }
+            });
         }
 
         super.mouseClicked(mouseX, mouseY, mouseButton);
-    }
-
-    @Override
-    protected void actionPerformed(GuiButton button) throws IOException {
-        if(button.id == 1337097) {
-            this.chatManager.setSelectedTab(this.chatManager.getOrCreateTabByName(this.textField.getText().trim()));
-            if(this.previous instanceof Chat) {
-                this.mc.displayGuiScreen(previous);
-            } else if(this.previous instanceof AvailableRooms) {
-                this.mc.displayGuiScreen(((AvailableRooms) this.previous).previous);
-            } else {
-                this.mc.displayGuiScreen(new Chat("", null, this.chocomint, this.mc.getCurrentServerData()));
-            }
-        } else {
-            super.actionPerformed(button);
-        }
     }
 
     @Override
@@ -102,37 +86,25 @@ public class UserSearch extends Menu {
             if(keyCode == 1) this.mc.displayGuiScreen(this.previous);
             this.textField.textboxKeyTyped(typedChar, keyCode);
 
+            this.filteredUserMap.clear();
+
             if(this.textField.getText().length() >= 3) {
                 this.almendra.getConnectedUsers().stream().filter(user -> user.toLowerCase()
-                        .startsWith(this.textField.getText().toLowerCase())).collect(Collectors.toList()).forEach(username ->
-                            this.filteredUserList.add(
-                                new UserCard(chocomint, this.mc, new User(username, User.Status.ONLINE, "", false, "default"),
-                                        true, false, (u) -> {
-                                            this.chatManager.setSelectedTab(this.chatManager.getOrCreateTabByName(username));
-                                            if(this.previous instanceof Chat) {
-                                                this.mc.displayGuiScreen(previous);
-                                            } else if(this.previous instanceof AvailableRooms) {
-                                                this.mc.displayGuiScreen(((AvailableRooms) this.previous).previous);
-                                            } else {
-                                                this.mc.displayGuiScreen(new Chat("", null, this.chocomint, this.mc.getCurrentServerData()));
-                                            }
-                                        }
-                                )
-                            )
+                        .startsWith(this.textField.getText().toLowerCase())).collect(Collectors.toList()).forEach(username -> {
+                            User user = new User(username, User.Status.ONLINE, "", false, "default");
+                            this.filteredUserMap.put(username,
+                                    new UserCard(chocomint, this.mc, user,true, false, (u) ->
+                                            this.mc.displayGuiScreen(new UserScreen(this.previous, this.chocomint, u)))
+                            );
+                        }
                 );
             }
 
         } else {
             if(!this.textField.getText().isEmpty()) {
                 if(this.almendra.getConnectedUsers().contains(this.textField.getText().trim()) && !this.textField.getText().trim().equalsIgnoreCase(this.chocomint.getUser().getUsername())) {
-                    this.chatManager.setSelectedTab(this.chatManager.getOrCreateTabByName(this.textField.getText().trim()));
-                    if(this.previous instanceof Chat) {
-                        this.mc.displayGuiScreen(previous);
-                    } else if(this.previous instanceof AvailableRooms) {
-                        this.mc.displayGuiScreen(((AvailableRooms) this.previous).previous);
-                    } else {
-                        this.mc.displayGuiScreen(new Chat("", null, this.chocomint, this.mc.getCurrentServerData()));
-                    }
+                    User user = new User(this.textField.getText().trim(), User.Status.ONLINE, "", false, "default");
+                    this.mc.displayGuiScreen(new UserScreen(this.previous, this.chocomint, user));
                 }
             }
         }
