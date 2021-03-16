e1bcd847b39367c6b5ba9f2c22043a66b8530375
Gerardo Wacker <ge@rar.vg>
2021-03-15 22:29:59 -0300

    Added account switcher, UIs, Threads, Managers and Configs

---
+++ src/main/java/io/undervolt/utils/MojangLoginThread.java
@@ -0,0 +1,77 @@
+package io.undervolt.utils;
+
+import com.mojang.authlib.Agent;
+import com.mojang.authlib.exceptions.AuthenticationException;
+import com.mojang.authlib.yggdrasil.YggdrasilAuthenticationService;
+import com.mojang.authlib.yggdrasil.YggdrasilUserAuthentication;
+
+import java.net.Proxy;
+
+import io.undervolt.utils.config.Config;
+import net.minecraft.client.Minecraft;
+import net.minecraft.util.Session;
+
+public class MojangLoginThread extends Thread {
+	
+    private final Minecraft mc;
+    private final String password;
+    private String status;
+    private final String username;
+    private final Config config;
+
+    public MojangLoginThread(final Config config, final Minecraft mc, String username, String password) {
+        super("Mojang Login Thread");
+        this.mc = mc;
+        this.config = config;
+        this.username = username;
+        this.password = password;
+        this.status = "\247eWaiting...";
+    }
+
+    private Session createSession(String username, String password) {
+        YggdrasilAuthenticationService service = new YggdrasilAuthenticationService(Proxy.NO_PROXY, "");
+        YggdrasilUserAuthentication auth = (YggdrasilUserAuthentication)service.createUserAuthentication(Agent.MINECRAFT);
+
+        auth.setUsername(username);
+        auth.setPassword(password);
+
+        try {
+            auth.logIn();
+            this.config.saveMinecraftCredentials(auth.saveForStorage());
+
+            return new Session(auth.getSelectedProfile().getName(), auth
+                    .getSelectedProfile().getId().toString(),
+                    auth.getAuthenticatedToken(), "mojang");
+        } catch (AuthenticationException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public String getStatus() {
+        return this.status;
+    }
+
+    public void run() {
+        if (this.password.equals("")) {
+            this.mc.setSession(new Session(this.username, "", "", "mojang"));
+            this.status = ("\247aIniciada la sesión. (" + this.username + " - no premium)");
+            return;
+        }
+
+        this.status = "\2471Iniciando sesión...";
+        Session auth = createSession(this.username, this.password);
+
+        if (auth == null)
+            this.status = "\2474Inicio de sesión fallido.";
+        else {
+            this.status = ("\247aIniciada la sesión. (" + auth.getUsername() + ")");
+            this.mc.setSession(auth);
+        }
+    }
+    
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
