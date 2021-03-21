6728ea074d09cec62f14afaf5a78001f4d120516
Gerardo Wacker <g@mscpn.com>
2021-03-21 00:56:18 -0300

    Added currentTab and returnMessage (Command.java)

--- src/main/java/io/undervolt/console/Command.java
+++ src/main/java/io/undervolt/console/Command.java
@@ -8,12 +8,14 @@ public class Command {
 
     private final String displayName, text, description;
     protected final Chocomint chocomint;
+    private final String chocomintUser;
 
     public Command(final Chocomint chocomint, final String displayName, final String text, final String description) {
         this.chocomint = chocomint;
         this.displayName = displayName;
         this.text = text;
         this.description = description;
+        this.chocomintUser = this.chocomint.getChocomintUser();
     }
 
     public void execute(final Tab tab, String[] args) { }
@@ -32,5 +34,13 @@ public class Command {
 
     protected Chocomint getChocomint() {
         return chocomint;
+    }
+
+    protected Tab getCurrentTab() {
+        return chocomint.getChatManager().getSelectedTab();
+    }
+
+    protected void returnMessage(final String message) {
+        this.getCurrentTab().addMessage(this.chocomintUser, message);
     }
 }
