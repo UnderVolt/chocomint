ea33f822226410a0ab01c6a4de3b403e3616fb7c
Gerardo Wacker <ge@rar.vg>
2021-09-24 10:18:23 -0300

    Added TextureGameBarButton based on Clickable API

--- src/main/java/io/undervolt/gui/clickable/TextureGameBarButton.java
+++ src/main/java/io/undervolt/gui/clickable/TextureGameBarButton.java
@@ -1,2 +1,35 @@
-package io.undervolt.gui.clickable;public class TextureGameBarButton {
+package io.undervolt.gui.clickable;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.Gui;
+import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
+
+import java.util.function.Consumer;
+
+public class TextureGameBarButton extends GameBarButton {
+
+    private ResourceLocation texture;
+
+    public TextureGameBarButton(int x, int y, int width, int height, String textureName, Consumer consumer) {
+        super(x, y, width, height, "", consumer);
+        this.texture = new ResourceLocation("/chocomint/icon/" + textureName + ".png");
+    }
+
+    public TextureGameBarButton(int x, int y, int width, int height, ResourceLocation texture, Consumer consumer) {
+        super(x, y, width, height, "", consumer);
+        this.texture = texture;
+    }
+
+    public void draw(int mouseX, int mouseY) {
+        super.draw(mouseX, mouseY);
+        this.mc.getTextureManager().bindTexture(this.texture);
+        GL11.glColor3f(255,255,255);
+        Gui.drawModalRectWithCustomSizedTexture(this.x + 2,this.y + 2,0,0,this.width - 4,
+                this.height - 4, this.width - 4, this.height - 4);
+    }
+
+    public void setTexture(ResourceLocation texture) {
+        this.texture = texture;
+    }
 }
