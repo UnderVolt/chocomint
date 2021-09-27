5cf2fee26578583eea6385b219f5010b6f7a818c
Gerardo Wacker <ge@rar.vg>
2021-09-27 12:00:26 -0300

    Added CircularGameButon

--- src/main/java/io/undervolt/gui/clickable/CircularGameBarButton.java
+++ src/main/java/io/undervolt/gui/clickable/CircularGameBarButton.java
@@ -1,2 +1,33 @@
-package io.undervolt.gui.clickable;public class CircularGameBarButton {
+package io.undervolt.gui.clickable;
+
+import io.undervolt.bridge.GameBridge;
+import io.undervolt.instance.Chocomint;
+
+import java.awt.*;
+import java.util.function.Consumer;
+
+public class CircularGameBarButton extends GameBarButton {
+
+    private final Chocomint chocomint;
+    private int radius;
+
+    public CircularGameBarButton(int x, int y, int radius, String buttonText, Consumer consumer) {
+        super(x, y, radius * 2, radius * 2, buttonText, consumer);
+        this.chocomint = GameBridge.getChocomint();
+        this.radius = radius;
+    }
+
+    @Override
+    public void draw(int mouseX, int mouseY) {
+        boolean isOverButton = (mouseX >= this.x) && (mouseX <= this.x + this.width) && (mouseY >= this.y) && (
+                mouseY <= this.y + this.height);
+        int c = new Color(32,34,37).getRGB();
+        if(enabled) {
+            if (isOverButton)
+                c = new Color(54, 57, 63).getRGB();
+        }
+
+        this.chocomint.getRenderUtils().drawFilledCircle(this.x + radius, this.y + radius, this.radius, c);
+        drawCenteredString(mc.fontRendererObj, this.buttonText, this.x + (this.width / 2), this.y + (this.height / 3), Color.WHITE.getRGB());
+    }
 }
