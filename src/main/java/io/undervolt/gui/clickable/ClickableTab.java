e1efb7b010025145839548288e04f6b0b1d77516
Gerardo Wacker <ge@rar.vg>
2021-09-29 11:55:51 -0300

    Added setTabName in ClickableTab

--- src/main/java/io/undervolt/gui/clickable/ClickableTab.java
+++ src/main/java/io/undervolt/gui/clickable/ClickableTab.java
@@ -16,15 +16,15 @@ public class ClickableTab extends Clickable {
     private final Chocomint chocomint;
     private final Minecraft mc;
     private final Tab tab;
+    private String tabName;
 
     public ClickableTab(int x, int y, Tab tab) {
         super(x, y, 50, 20, null);
         this.chocomint = GameBridge.getChocomint();
         this.mc = GameBridge.getMinecraft();
         this.tab = tab;
-        this.consumer = (a) -> {
-            this.chocomint.getChatManager().setSelectedTab(tab);
-        };
+        this.tabName = tab.getName();
+        this.consumer = (a) -> this.chocomint.getChatManager().setSelectedTab(tab);
     }
 
     @Override
@@ -34,13 +34,13 @@ public class ClickableTab extends Clickable {
 
         boolean mouseOverCloseButton = (mouseX > x + width - 15 && mouseY > y + 5 && mouseX < x + width - 6 && mouseY < y + 15);
 
-        this.width = 24 + this.mc.fontRendererObj.getStringWidth(tab.getName());
+        this.width = 24 + this.mc.fontRendererObj.getStringWidth(this.tabName);
         this.chocomint.getRenderUtils().drawRoundedRect(x, y, x + width, y + height,
                 8, 8, 0, 0,
                 this.chocomint.getChatManager().getSelectedTab().equals(this.tab) ?  new Color(79, 82, 92).getRGB()
                         : (isMouseOver ?  new Color(54,57,63).getRGB() : new Color(32,34,37).getRGB()));
         GL11.glColor3f(255, 255, 255);
-        this.mc.fontRendererObj.drawString(tab.getName(), x + 5, y + 7, tab.isRead() ? Color.WHITE.getRGB() :  new Color(255, 218, 108).getRGB());
+        this.mc.fontRendererObj.drawString(this.tabName, x + 5, y + 7, tab.isRead() ? Color.WHITE.getRGB() :  new Color(255, 218, 108).getRGB());
         if(!tab.getName().equals("SERVER_RESERVED")) {
             if(isMouseOver) {
                 this.chocomint.getRenderUtils().drawFilledCircle(x + width - 10, y + 11, 4,
@@ -51,6 +51,8 @@ public class ClickableTab extends Clickable {
                 this.mc.fontRendererObj.drawString("✕", 0, 0, Color.WHITE.getRGB());
                 GL11.glPopMatrix();
             }
+        } else {
+            drawRect(x + width + 8, y + 4, x + width + 9, y + height - 2, new Color(200, 200, 200).getRGB());
         }
     }
 
@@ -60,5 +62,9 @@ public class ClickableTab extends Clickable {
             this.chocomint.getChatManager().removeTab(tab);
             ((Chat) this.mc.currentScreen).clickablesToRemove.add(tab.getName());
         } else super.click(mouseX, mouseY);
+    }
+
+    public void setTabName(String tabName) {
+        this.tabName = tabName;
     }
 }
