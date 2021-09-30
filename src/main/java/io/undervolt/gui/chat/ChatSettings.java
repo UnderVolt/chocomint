2e6faf352e8b48f645764ecdeee01d9f908f6bf2
Gerardo Wacker <ge@rar.vg>
2021-09-30 10:45:35 -0300

    Added content to ChatSettings class

--- src/main/java/io/undervolt/gui/chat/ChatSettings.java
+++ src/main/java/io/undervolt/gui/chat/ChatSettings.java
@@ -1,2 +1,20 @@
-package io.undervolt.gui.chat;public class ChatSettings {
+package io.undervolt.gui.chat;
+
+import io.undervolt.utils.config.Configurable;
+
+public class ChatSettings extends Configurable {
+
+    public int chatHeight = 100;
+
+    public ChatSettings() {
+        super("Chat");
+    }
+
+    public int getChatHeight() {
+        return chatHeight;
+    }
+
+    public void setChatHeight(int chatHeight) {
+        this.chatHeight = chatHeight;
+    }
 }
