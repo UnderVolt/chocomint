e3c071cefe8b3f9c1a065add0e6a91a8021167f8
Gerardo Wacker <ge@rar.vg>
2021-10-02 12:49:11 -0300

    Added clearNotificationsButton in NotificationsPanel

--- src/main/java/io/undervolt/gui/notifications/NotificationScreen.java
+++ src/main/java/io/undervolt/gui/notifications/NotificationScreen.java
@@ -1,14 +1,18 @@
 package io.undervolt.gui.notifications;
 
 import io.undervolt.gui.Panel;
+import io.undervolt.gui.clickable.ClickableLabel;
 import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.util.ResourceLocation;
 
+import java.awt.*;
+import java.io.IOException;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class NotificationScreen extends Panel {
 
     private final NotificationManager notificationManager;
+    private ClickableLabel clearNotificationsButton;
 
     public NotificationScreen(GuiScreen previousScreen) {
         super(previousScreen, "Notificaciones", previousScreen.height);
@@ -20,11 +24,15 @@ public class NotificationScreen extends Panel {
         this.chocomint.getGameBar().notificationsButton.setTexture(
                 new ResourceLocation("/chocomint/icon/notifications.png")
         );
+        this.clearNotificationsButton = new ClickableLabel(
+                this.width - this.mc.fontRendererObj.getStringWidth("LIMPIAR") - 5, 28, "LIMPIAR",
+                a -> this.notificationManager.clearNotifications());
         super.initGui();
     }
 
     @Override
     public void drawContent(int mouseX, int mouseY, float partialTicks, int margin, int scroll) {
+        this.clearNotificationsButton.draw(mouseX, mouseY);
         AtomicInteger y = new AtomicInteger(scroll);
         this.notificationManager.getNotifications().forEach(notification -> {
             notification.draw(this.mc, margin + 5, y.get() + 50, this.getPanelWidth() - 10);
@@ -34,5 +42,9 @@ public class NotificationScreen extends Panel {
         this.pageSize = y.get();
     }
 
-
+    @Override
+    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+        this.clearNotificationsButton.click(mouseX, mouseY, mouseButton);
+        super.mouseClicked(mouseX, mouseY, mouseButton);
+    }
 }
