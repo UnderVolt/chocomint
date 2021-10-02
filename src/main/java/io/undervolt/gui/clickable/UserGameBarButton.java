6137fb6ba0bea627d24355ba533d008262f20a7a
Gerardo Wacker <ge@rar.vg>
2021-10-01 23:10:13 -0300

    UserGameBarButton now displays Alias instead of username

--- src/main/java/io/undervolt/gui/clickable/UserGameBarButton.java
+++ src/main/java/io/undervolt/gui/clickable/UserGameBarButton.java
@@ -26,7 +26,7 @@ public class UserGameBarButton extends GameBarButton {
     @Override
     public void draw(int mouseX, int mouseY) {
         super.draw(mouseX, mouseY);
-        this.width = 20 + this.mc.fontRendererObj.getStringWidth(this.user.getUsername());
+        this.width = 20 + this.mc.fontRendererObj.getStringWidth(this.user.getAlias());
         this.mc.getTextureManager().bindTexture(this.pictureLocation);
         GL11.glColor3f(255,255,255);
         Gui.drawModalRectWithCustomSizedTexture(this.x + 2,this.y + 2,0,0,16,
@@ -35,7 +35,7 @@ public class UserGameBarButton extends GameBarButton {
         GL11.glPushMatrix();
         GlStateManager.translate(this.x + 23, this.y + 7.5, 0);
         GL11.glScalef(0.75F, 0.75F, 0);
-        this.mc.fontRendererObj.drawString(user.getUsername(), 0, 0, Color.WHITE.getRGB());
+        this.mc.fontRendererObj.drawString(user.getAlias(), 0, 0, Color.WHITE.getRGB());
         GL11.glPopMatrix();
 
     }
