a616538bb99e73626340a276ff2859125d9a8d30
Gerardo Wacker <ge@rar.vg>
2021-10-22 17:02:51 -0300

    Added reloadScreen method to Screen

--- src/main/java/io/undervolt/api/ui/Screen.java
+++ src/main/java/io/undervolt/api/ui/Screen.java
@@ -17,185 +17,202 @@ import java.util.List;
 
 public abstract class Screen extends AnimationUI {
 
-    protected Drawable[] children;
-    private List<Drawable> widgets = Lists.newArrayList();
+   protected Drawable[] children;
+   private List<Drawable> widgets = Lists.newArrayList();
 
-    public final Minecraft minecraft = Minecraft.getMinecraft();
-    public final Chocomint chocomint = GameBridge.getChocomint();
-    public float oldTime;
-    public float newTime;
-    public float deltaTime;
-    protected ScaledResolution sr;
-    protected boolean forceUseScaleFactor = true;
-    protected int forcedScaleFactor = 2;
-    private boolean startedDrawing = false;
-    private long time = 0;
-    private int tmpMouseX = 0;
-    private int tmpMouseY = 0;
+   public final Minecraft minecraft = Minecraft.getMinecraft();
+   public final Chocomint chocomint = GameBridge.getChocomint();
+   public float oldTime;
+   public float newTime;
+   public float deltaTime;
+   protected ScaledResolution sr;
+   protected boolean forceUseScaleFactor = true;
+   protected int forcedScaleFactor = 2;
+   private boolean startedDrawing = false;
+   private long time = 0;
+   private int tmpMouseX = 0;
+   private int tmpMouseY = 0;
 
-    public void load(){}
-    public void update(int mouseX, int mouseY, float deltaTime){}
-    protected void update() {}
-    public void closeUI(){}
+   public void load() {
+   }
 
-    @Override
-    public void initGui() {
-        this.oldTime = this.newTime;
-        this.newTime = Minecraft.getSystemTime() / 1000.0f;
-        this.load();
-        this.addWidgets(children);
-        this.deltaTime = this.newTime - this.oldTime;
-    }
+   public void update(int mouseX, int mouseY, float deltaTime) {
+   }
 
-    @Override
-    public void setWorldAndResolution(Minecraft mc, int width, int height) {
-        this.itemRender = mc.getRenderItem();
-        this.fontRendererObj = mc.fontRendererObj;
-        this.buttonList.clear();
-        this.widgets.clear();
-        this.mc = mc;
-        this.sr = new ScaledResolution(this.mc);
-        if(this.forceUseScaleFactor){
-            this.width = (int)Math.floor(Display.getWidth() / (float)this.forcedScaleFactor);
-            this.height = (int)Math.floor(Display.getHeight() / (float)this.forcedScaleFactor);
-        }else{
-            this.width = this.sr.getScaledWidth();
-            this.height = this.sr.getScaledHeight();
-        }
-        this.initGui();
-    }
+   protected void update() {
+   }
 
-    @Override
-    public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-        this.oldTime = this.newTime;
-        this.newTime = Minecraft.getSystemTime() / 1000.0f;
+   public void closeUI() {
+   }
 
-        this.sr = new ScaledResolution(this.mc);
+   @Override
+   public void initGui() {
+      this.oldTime = this.newTime;
+      this.newTime = Minecraft.getSystemTime() / 1000.0f;
+      this.load();
+      this.addWidgets(children);
+      this.deltaTime = this.newTime - this.oldTime;
+   }
 
-        if(this.forceUseScaleFactor){
-            GL11.glPushMatrix();
-            GL11.glDisable(GL11.GL_BLEND);
-            GL11.glColor3f(1, 1, 1);
+   @Override
+   public void setWorldAndResolution(Minecraft mc, int width, int height) {
+      this.itemRender = mc.getRenderItem();
+      this.fontRendererObj = mc.fontRendererObj;
+      this.buttonList.clear();
+      this.widgets.clear();
+      this.mc = mc;
+      this.sr = new ScaledResolution(this.mc);
+      if (this.forceUseScaleFactor) {
+         this.width = (int) Math.floor(Display.getWidth() / (float) this.forcedScaleFactor);
+         this.height = (int) Math.floor(Display.getHeight() / (float) this.forcedScaleFactor);
+      } else {
+         this.width = this.sr.getScaledWidth();
+         this.height = this.sr.getScaledHeight();
+      }
+      this.initGui();
+   }
 
-            float scaleWidthFactor = this.sr.getScaledWidth() / (Display.getWidth() / (float)this.forcedScaleFactor);
-            float scaleHeightFactor = this.sr.getScaledHeight() / (Display.getHeight() / (float)this.forcedScaleFactor);
+   @Override
+   public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+      this.oldTime = this.newTime;
+      this.newTime = Minecraft.getSystemTime() / 1000.0f;
 
-            GL11.glScalef(scaleWidthFactor, scaleHeightFactor, 1);
+      this.sr = new ScaledResolution(this.mc);
 
-            this.tmpMouseX = (int)Math.floor(mouseX / scaleWidthFactor);
-            this.tmpMouseY = (int)Math.floor(mouseY / scaleHeightFactor);
+      if (this.forceUseScaleFactor) {
+         GL11.glPushMatrix();
+         GL11.glDisable(GL11.GL_BLEND);
+         GL11.glColor3f(1, 1, 1);
 
-            if(!this.startedDrawing){
-                this.time = System.currentTimeMillis();
-                this.startedDrawing = true;
-            }
+         float scaleWidthFactor = this.sr.getScaledWidth() / (Display.getWidth() / (float) this.forcedScaleFactor);
+         float scaleHeightFactor = this.sr.getScaledHeight() / (Display.getHeight() / (float) this.forcedScaleFactor);
 
-            super.drawScreen(this.tmpMouseX , this.tmpMouseY, partialTicks);
-            this.widgets.forEach(w -> w.draw(this, 0, 0, this.tmpMouseX, this.tmpMouseY, deltaTime));
-            this.update(this.tmpMouseX, this.tmpMouseY, this.deltaTime);
-            this.update();
-            this.deltaTime = this.newTime - this.oldTime;
-            GL11.glPopMatrix();
-        }else{
-            if(!this.startedDrawing){
-                this.time = System.currentTimeMillis();
-                this.startedDrawing = true;
-            }
+         GL11.glScalef(scaleWidthFactor, scaleHeightFactor, 1);
 
-            this.tmpMouseX = mouseX;
-            this.tmpMouseY = mouseY;
+         this.tmpMouseX = (int) Math.floor(mouseX / scaleWidthFactor);
+         this.tmpMouseY = (int) Math.floor(mouseY / scaleHeightFactor);
 
-            GL11.glDisable(GL11.GL_BLEND);
-            GL11.glColor3f(1, 1, 1);
-            super.drawScreen(mouseX, mouseY, partialTicks);
-            this.widgets.forEach(w -> w.draw(this, 0, 0, mouseX, mouseY, deltaTime));
-            this.update(mouseX, mouseY, this.deltaTime);
-            this.update();
-            this.deltaTime = this.newTime - this.oldTime;
-        }
-    }
+         if (!this.startedDrawing) {
+            this.time = System.currentTimeMillis();
+            this.startedDrawing = true;
+         }
 
-    public void addWidgets(Drawable... widgets){
-        if(widgets != null)
-            for (Drawable w : widgets) {
-                this.widgets.add(new Container(this.getWidth(), this.getHeight()).setChild(w));
-                w.init();
-            }
-    }
+         super.drawScreen(this.tmpMouseX, this.tmpMouseY, partialTicks);
+         this.widgets.forEach(w -> w.draw(this, 0, 0, this.tmpMouseX, this.tmpMouseY, deltaTime));
+         this.update(this.tmpMouseX, this.tmpMouseY, this.deltaTime);
+         this.update();
+         this.deltaTime = this.newTime - this.oldTime;
+         GL11.glPopMatrix();
+      } else {
+         if (!this.startedDrawing) {
+            this.time = System.currentTimeMillis();
+            this.startedDrawing = true;
+         }
 
-    @Override
-    protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
-        this.widgets.forEach(w -> w.onPress(this.tmpMouseX, this.tmpMouseY, mouseButton));
+         this.tmpMouseX = mouseX;
+         this.tmpMouseY = mouseY;
 
-        super.mouseClicked(this.tmpMouseX, this.tmpMouseY, mouseButton);
-    }
+         GL11.glDisable(GL11.GL_BLEND);
+         GL11.glColor3f(1, 1, 1);
+         super.drawScreen(mouseX, mouseY, partialTicks);
+         this.widgets.forEach(w -> w.draw(this, 0, 0, mouseX, mouseY, deltaTime));
+         this.update(mouseX, mouseY, this.deltaTime);
+         this.update();
+         this.deltaTime = this.newTime - this.oldTime;
+      }
+   }
 
-    @Override
-    protected void mouseReleased(int mouseX, int mouseY, int state) {
+   public void addWidgets(Drawable... widgets) {
+      if (widgets != null)
+         for (Drawable w : widgets) {
+            System.out.println(w.getWidth());
+            this.widgets.add(new Container(this.getWidth(), this.getHeight()).setChild(w));
+            w.init();
+         }
+   }
 
-        this.widgets.forEach(w -> w.onRelease(this.tmpMouseX, this.tmpMouseY, state));
+   protected void reloadScreen() {
+      this.reloadScreen(children);
+   }
 
-        super.mouseReleased(this.tmpMouseX, this.tmpMouseY, state);
-    }
+   protected void reloadScreen(Drawable[] children) {
+      this.widgets.clear();
+      this.addWidgets(children);
+   }
 
-    @Override
-    protected void mouseClickMove(int mouseX, int mouseY, int clickedMouseButton, long timeSinceLastClick) {
-        this.widgets.forEach(w -> w.onDrag(this.tmpMouseX, this.tmpMouseY, clickedMouseButton, timeSinceLastClick));
+   @Override
+   protected void mouseClicked(int mouseX, int mouseY, int mouseButton) throws IOException {
+      this.widgets.forEach(w -> w.onPress(this.tmpMouseX, this.tmpMouseY, mouseButton));
 
-        super.mouseClickMove(this.tmpMouseX, this.tmpMouseY, clickedMouseButton, timeSinceLastClick);
-    }
+      super.mouseClicked(this.tmpMouseX, this.tmpMouseY, mouseButton);
+   }
 
-    @Override
-    public void onGuiClosed() {
-        this.closeUI();
-    }
+   @Override
+   protected void mouseReleased(int mouseX, int mouseY, int state) {
 
-    public void forceScaleFactor(int newScaleFactor){
-        this.forceUseScaleFactor = true;
-        this.forcedScaleFactor = newScaleFactor;
-    }
+      this.widgets.forEach(w -> w.onRelease(this.tmpMouseX, this.tmpMouseY, state));
 
-    public boolean forcingUseScaleFactor() {
-        return forceUseScaleFactor;
-    }
+      super.mouseReleased(this.tmpMouseX, this.tmpMouseY, state);
+   }
 
-    public int getWidth(){
-        return Math.round(this.forceUseScaleFactor ? (Display.getWidth() / (float)this.forcedScaleFactor) : this.sr.getScaledWidth());
-    }
+   @Override
+   protected void mouseClickMove(int mouseX, int mouseY, int clickedMouseButton, long timeSinceLastClick) {
+      this.widgets.forEach(w -> w.onDrag(this.tmpMouseX, this.tmpMouseY, clickedMouseButton, timeSinceLastClick));
 
-    public int getHeight(){
-        return Math.round(this.forceUseScaleFactor ? (Display.getHeight() / (float)this.forcedScaleFactor) : this.sr.getScaledHeight());
-    }
+      super.mouseClickMove(this.tmpMouseX, this.tmpMouseY, clickedMouseButton, timeSinceLastClick);
+   }
 
-    public float getWidthA(){
-        return this.forceUseScaleFactor ? Display.getHeight() : this.sr.getScaledWidth();
-    }
+   @Override
+   public void onGuiClosed() {
+      this.closeUI();
+   }
 
-    public float getHeightA(){
-        return this.forceUseScaleFactor ? Display.getWidth() : this.sr.getScaledHeight();
-    }
+   public void forceScaleFactor(int newScaleFactor) {
+      this.forceUseScaleFactor = true;
+      this.forcedScaleFactor = newScaleFactor;
+   }
 
-    public int getScaleFactor(){
-        return this.forceUseScaleFactor ? this.forcedScaleFactor : this.sr.getScaleFactor();
-    }
+   public boolean forcingUseScaleFactor() {
+      return forceUseScaleFactor;
+   }
 
-    public float getFixedWidth(float originalWidth){
-        return this.getWidth() * (1.0f / (this.getWidth() / (originalWidth / (this.forceUseScaleFactor ? this.forcedScaleFactor : sr.getScaleFactor()))));
-    }
+   public int getWidth() {
+      return Math.round(this.forceUseScaleFactor ? (Display.getWidth() / (float) this.forcedScaleFactor) : this.sr.getScaledWidth());
+   }
 
-    public float getFixedHeight(float originalWidth){
-        return this.getHeight() * (1.0f / (this.getHeight() / (originalWidth / (this.forceUseScaleFactor ? this.forcedScaleFactor : sr.getScaleFactor()))));
-    }
+   public int getHeight() {
+      return Math.round(this.forceUseScaleFactor ? (Display.getHeight() / (float) this.forcedScaleFactor) : this.sr.getScaledHeight());
+   }
 
-    /**
-     * Returns the time in seconds since the UI Init
-     *
-     * @return      The time in seconds
-     */
+   public float getWidthA() {
+      return this.forceUseScaleFactor ? Display.getHeight() : this.sr.getScaledWidth();
+   }
 
-    public float getTime(){
-        return (float) ((System.currentTimeMillis() - this.time) / 1000.0);
-    }
+   public float getHeightA() {
+      return this.forceUseScaleFactor ? Display.getWidth() : this.sr.getScaledHeight();
+   }
+
+   public int getScaleFactor() {
+      return this.forceUseScaleFactor ? this.forcedScaleFactor : this.sr.getScaleFactor();
+   }
+
+   public float getFixedWidth(float originalWidth) {
+      return this.getWidth() * (1.0f / (this.getWidth() / (originalWidth / (this.forceUseScaleFactor ? this.forcedScaleFactor : sr.getScaleFactor()))));
+   }
+
+   public float getFixedHeight(float originalWidth) {
+      return this.getHeight() * (1.0f / (this.getHeight() / (originalWidth / (this.forceUseScaleFactor ? this.forcedScaleFactor : sr.getScaleFactor()))));
+   }
+
+   /**
+    * Returns the time in seconds since the UI Init
+    *
+    * @return The time in seconds
+    */
+
+   public float getTime() {
+      return (float) ((System.currentTimeMillis() - this.time) / 1000.0);
+   }
 
 }
 
