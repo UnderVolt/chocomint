a3ceeb958dec3afb36d0255be063f41978d21e10
Gerardo Wacker <ge@rar.vg>
2021-10-25 22:52:41 -0300

    Added BasicScreen class

--- src/main/java/io/undervolt/api/ui/BasicScreen.java
+++ src/main/java/io/undervolt/api/ui/BasicScreen.java
@@ -1,2 +1,13 @@
-package io.undervolt.api.ui;public class BasicScreen {
+package io.undervolt.api.ui;
+
+import io.undervolt.utils.Colour;
+
+public class BasicScreen extends Screen {
+
+   @Override
+   public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+      drawRect(0, 0, width, height, Colour.Black.getRGB());
+      super.drawScreen(mouseX, mouseY, partialTicks);
+   }
 }
+
