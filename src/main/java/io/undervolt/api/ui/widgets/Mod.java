7a738896258819ab5275fcb3baf7617b14955be5
Gerardo Wacker <ge@rar.vg>
2021-10-31 14:56:48 -0300

    Render and Draw methods are now separated in Mod Drawable

--- src/main/java/io/undervolt/api/ui/widgets/Mod.java
+++ src/main/java/io/undervolt/api/ui/widgets/Mod.java
@@ -7,6 +7,7 @@ import org.lwjgl.opengl.GL11;
 public class Mod extends Drawable {
 
    public RenderMod renderMod;
+   protected boolean visible = true;
 
    /**
     * Drawable variant for RenderMods
@@ -15,15 +16,29 @@ public class Mod extends Drawable {
    public Mod(RenderMod renderMod)
    {
       this.renderMod = renderMod;
-      this.width = 100;
-      this.height = 12;
+      this.width = renderMod.width;
+      this.height = renderMod.height;
    }
 
    @Override public void draw(Screen ui, int x, int y, int mouseX, int mouseY, float deltaTime)
    {
+      if(visible)
+      {
+         this.render(ui, x, y, mouseX, mouseY, deltaTime);
+      }
+      super.draw(ui, x, y, mouseX, mouseY, deltaTime);
+   }
+
+   public void render(Screen ui, int x, int y, int mouseX, int mouseY, float deltaTime)
+   {
       GL11.glPushMatrix();
       GL11.glTranslatef(x, y, 0);
+      GL11.glScalef(this.renderMod.scale, this.renderMod.scale, 1);
       this.renderMod.render();
       GL11.glPopMatrix();
+   }
+
+   public void setVisible(boolean visible) {
+      this.visible = visible;
    }
 }
